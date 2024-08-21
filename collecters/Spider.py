355f0ae7564dcfae3d8ab3098fde2b3cc3424512
Ashot Msryan <amsryan@c2r5s3.42yerevan.am>
2024-08-21 20:23:59 +0400

    using bs4 | dowloads

--- collecters/Spider.py
+++ collecters/Spider.py
@@ -1,6 +1,7 @@
 import argparse
-from selenium import webdriver
+from bs4 import BeautifulSoup
 import requests
+import os
 
 
 def recursiveSearch(args):
@@ -12,29 +13,50 @@ def dephtSetiing(args):
 def saveingInPath(args):
     print ("p is working")
 
-def main():
+def getReqImg(name):
+    n = name
     ext = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
+    lastDot = name.rfind('.')
+    if (lastDot != -1 and n[lastDot:len(n)] in ext):
+        return True
+    return False
+        
+def main():
     # try:
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--recursive", dest="rec", action="store_true", help="Recursivly read in all directories")
     parser.add_argument("-l", "--level", dest="lvl", type=int, nargs='?', const=5, help="Sets the depht of search")
     parser.add_argument("-p", "--path", dest="pth", type=str, nargs='?', const="./data/", help="The path of page")
     parser.add_argument("url", type=str, help="The URL to process")
+    
     args = parser.parse_args()
-    driver = webdriver.Chrome(executable_path="~/Applications/Chrome")
-    print("hasnuma")
-    driver.get(args.url)
-    img = driver.find_element(By.XPATH, "//div[matches(@class, '.*\.jpg')]")
-    img_url = img.get_attribute("src")
-    response = requests.get(img_url)
-    with open('image.jpg', 'wb') as file:
-        file.write(response.content) 
-    if args.rec:
-        recursiveSearch(args)
-    if args.lvl:
-        dephtSetiing(args)
-    if args.pth:
-        saveingInPath(args)
+
+    response = requests.get(args.url)
+    html_cont = response.text
+    soup = BeautifulSoup(html_cont, 'html.parser')
+    imgs = soup.find_all('img')
+
+    for img in imgs:
+        img_url = img.get('src')
+        if not img_url:
+            continue
+        if not img_url.startswith(('http:', 'https:')):
+            img_url = requests.compat.urljoin(args.url, img_url)
+        
+        name = os.path.basename(img_url)
+        if (getReqImg(name) == False):
+            continue
+        result = requests.get(img_url)
+
+        with open(name, 'wb') as f:
+            f.write(result.content)
+
+    # if args.rec:
+    #     recursiveSearch(args)
+    # if args.lvl:
+    #     dephtSetiing(args)
+    # if args.pth:
+    #     saveingInPath(args)
     # except Exception as e:
         # print("ape stop: ".format(e))
 
