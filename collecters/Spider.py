26eeab34ece83df7bb847d0c5aa814ec1d9480e3
Ashot Msryan <amsryan@c2r12s6.42yerevan.am>
2024-09-10 20:39:48 +0400

    recursion not working

--- collecters/Spider.py
+++ collecters/Spider.py
@@ -4,34 +4,36 @@ import requests
 import os
 
 
-def recursiveSearch(args, lvl, pth):
-    response = requests.get(args.url)
+def recursiveSearch(url, lvl, pth, i):
+    if lvl == 0:
+        return
+    if not url:
+        return
+    response = requests.get(url)
     html_cont = response.text
     soup = BeautifulSoup(html_cont, 'html.parser')
     imgs = soup.find_all('img')
+    links = soup.find_all('a href')
     os.makedirs(os.path.dirname(pth), exist_ok=True)
-
+    
     for img in imgs:
         img_url = img.get('src')
         if not img_url:
             continue
         if not img_url.startswith(('http:', 'https:')):
-            img_url = requests.compat.urljoin(args.url, img_url)
-        
+            img_url = requests.compat.urljoin(url, img_url)
         name = os.path.basename(img_url)
         if (getReqImg(name) == False):
             continue
         result = requests.get(img_url)
-        # if (os.path.exists(path) or os.path.isdir(os.path.dirname(path))):
         name = pth + name
         with open(name, 'wb') as f:
             f.write(result.content)
-
-def dephtSetiing(args):
-    print ("l is working")
-
-def saveingInPath(args):
-    print ("p is working")
+    if not links:
+        return
+    #chgitem vonc grem es rekursian
+    recursiveSearch(links[i], lvl, pth, i + 1)
+    recursiveSearch(links[i], lvl - 1, pth, i + 1)
 
 def getReqImg(name):
     n = name
@@ -62,7 +64,7 @@ def main():
         path = args.pth
     print (path)
     if args.rec:
-        recursiveSearch(args, level, path)
+        recursiveSearch(args.url, level, path, i = 0)
 
     # except Exception as e:
         # print("ape stop: ".format(e))
